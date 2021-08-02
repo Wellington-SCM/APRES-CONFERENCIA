78f5a1d3bd1b9f957f916c9149dc6a540d218163
Wellington-SCM <88327719+Wellington-SCM@users.noreply.github.com>
2021-08-02 09:58:02 -0300

    Add files via upload

---
+++ CapacitaConf/js/index.js
@@ -0,0 +1,16 @@
+function ativaScrollSuave(selector) {
+
+    $(selector).click(function(event){
+        event.preventDefault();
+
+        var target = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 5000);  
+    });
+}
+
+ativaScrollSuave('a[href*=event-conf');
+ativaScrollSuave('a[href*=theSpeakers]');
+ativaScrollSuave('a[href*=formulary]');
